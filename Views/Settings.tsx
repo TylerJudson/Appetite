28106993b4e0b9b7c98131fbfa7c800de222e68c
Tyler Judson <tylerjudson23@gmail.com>
2022-11-23 20:05:46 -0700

    Re-Initial

--- Views/Settings.tsx
+++ Views/Settings.tsx
@@ -5,7 +5,7 @@ export default function Settings() {
     const colors = theme.colors;
     
     return (
-        <Text variant="headlineLarge" >Settings -- this is a </Text>
+        <Text variant="headlineLarge" >Settings</Text>
     );
 }
 
