9654c55841f157a9782208c6aac760674bedd992
Tyler Judson <tylerjudson23@gmail.com>
2023-01-02 10:59:23 -0700

    Documented a utility

--- utilities/importToObject.ts
+++ utilities/importToObject.ts
@@ -1,5 +1,9 @@
 
-// TODO: Docs
+/**
+ * Imports data into a reference object
+ * @param ref The reference of the object to import
+ * @param data the data to import into the object THIS MUST BE THE SAME TYPE!
+ */
 export function importToObject(ref: any, data: any) {
     Object.getOwnPropertyNames(ref).forEach(key => {
         ref[key] = data[key];
